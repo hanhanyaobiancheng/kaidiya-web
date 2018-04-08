60891ae6e56edb3d208ea26b841453c6097875a7
linhan <linhan1991@126.com>
2018-04-08 09:18:39 +0800

    去掉一些依赖

--- antd/table/Table.jsx
+++ antd/table/Table.jsx
@@ -1,72 +1,8 @@
 import React, {Component} from 'react';
 import PropType from 'prop-types';
 import {Table} from 'antd';
-import {DragDropContext, DragSource, DropTarget} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-import update from 'immutability-helper';
-import * as style from './style.less';
 
-
-function dragDirection(
-    dragIndex,
-    hoverIndex,
-    initialClientOffset,
-    clientOffset,
-    sourceClientOffset,
-) {
-    const hoverMiddleY = (initialClientOffset.y - sourceClientOffset.y) / 2;
-    const hoverClientY = clientOffset.y - sourceClientOffset.y;
-    if (dragIndex < hoverIndex && hoverClientY > hoverMiddleY) {
-        return 'downward';
-    }
-    if (dragIndex > hoverIndex && hoverClientY < hoverMiddleY) {
-        return 'upward';
-    }
-}
-
-let BodyRow = (props) => {
-    const {
-        isOver,
-        connectDragSource,
-        connectDropTarget,
-        moveRow,
-        dragRow,
-        clientOffset,
-        sourceClientOffset,
-        initialClientOffset,
-        ...restProps
-    } = props;
-    const style = {...restProps.style, cursor: 'move'};
-
-    let className = restProps.className;
-    if (isOver && initialClientOffset) {
-        const direction = this.dragDirection(
-            dragRow.index,
-            restProps.index,
-            initialClientOffset,
-            clientOffset,
-            sourceClientOffset
-        );
-        if (direction === 'downward') {
-            className += ' drop-over-downward';
-        }
-        if (direction === 'upward') {
-            className += ' drop-over-upward';
-        }
-    }
-
-    return connectDragSource(
-        connectDropTarget(
-            <tr
-                {...restProps}
-                className={className}
-                style={style}
-            />
-        )
-    );
-};
-
-class TableDrag extends Component {
+export default class TablePro extends Component {
     static defaultProps = {};
 
     static propTypes = {
@@ -89,58 +25,13 @@ class TableDrag extends Component {
         }
     }
 
-    rowSource = {
-        beginDrag(props) {
-            return {
-                index: props.index,
-            };
-        },
-    };
-
-    rowTarget = {
-        drop(props, monitor) {
-            const dragIndex = monitor.getItem().index;
-            const hoverIndex = props.index;
-
-            // Don't replace items with themselves
-            if (dragIndex === hoverIndex) {
-                return;
-            }
-
-            // Time to actually perform the action
-            props.moveRow(dragIndex, hoverIndex);
-
-            // Note: we're mutating the monitor item here!
-            // Generally it's better to avoid mutations,
-            // but it's good here for the sake of performance
-            // to avoid expensive index searches.
-            monitor.getItem().index = hoverIndex;
-        },
-    };
-
-    BodyRow = DropTarget('row', this.rowTarget, (connect, monitor) => ({
-        connectDropTarget: connect.dropTarget(),
-        isOver: monitor.isOver(),
-        sourceClientOffset: monitor.getSourceClientOffset(),
-    }))(
-        DragSource('row', this.rowSource, (connect, monitor) => ({
-            connectDragSource: connect.dragSource(),
-            dragRow: monitor.getItem(),
-            clientOffset: monitor.getClientOffset(),
-            initialClientOffset: monitor.getInitialClientOffset(),
-        }))(BodyRow)
-    );
 
     render() {
         const {
             columns,
             components,
-        } = this.props;
-
-
-        const {
             dataSource,
-        } = this.state;
+        } = this.props;
 
         return (
             <div className="wrap">
@@ -160,7 +51,3 @@ class TableDrag extends Component {
         );
     }
 }
-
-const TablePro = DragDropContext(HTML5Backend)(TableDrag);
-
-export default TablePro;
